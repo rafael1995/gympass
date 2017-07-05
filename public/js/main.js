3b1a31f0ac8925eeff4a788e08527708b20de9b7
Rafael Alves Silva <rafael.alvessilva1995@gmail.com>
2017-07-05 10:11:39 -0300

    Ajuste em identações e inclusão ownl Carousel

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,51 @@
- $( document ).ready(function(){ 
+ $(document).ready(function() {
 
-// Inicializando menu - Mobile
- $(".button-collapse").sideNav();
+     // Inicializando menu - Mobile
+     $(".button-collapse").sideNav();
 
- //
-  $('').change(function() {
-      
-  });
-var aquatica = true;
-var notAquatica = true;
-//
-listaModalidades(aquatica,notAquatica);
-}); // end Ready
+     //
+     $('').change(function() {
 
- function listaModalidades(aquatica,notAquatica){
-    json = $.getJSON("classes.json",function(obj) {
-            for( i = 0; i < obj.classes.length;i++){
-                if(notAquatica && aquatica){
-                    console.log("tudo");
-                }
-                else if(!notAquatica && aquatica){
-                    console.log("Apenas aquatica");
-                }
-                 else if(notAquatica && !aquatica){
-                    console.log("Apenas Não Aquatica");
-                } else{
-                    console.log("tudo");
-                }
-            }// end for
-        });// end getJson
-}
-        
+     });
+     var aquatica = true;
+     var notAquatica = true;
+     //
+     listaModalidades(aquatica, notAquatica);
+     // Inicializando  Owl Carousel 
+     $('.owl-carousel').owlCarousel({
+         loop: true,
+         margin: 10,
+         responsiveClass: true,
+         responsive: {
+             0: {
+                 items: 1,
+                 nav: true
+             },
+             600: {
+                 items: 3,
+                 nav: false
+             },
+             1000: {
+                 items: 5,
+                 nav: true,
+                 loop: false
+             }
+         }
+     });
+ }); // end Ready
+
+ function listaModalidades(aquatica, notAquatica) {
+     json = $.getJSON("classes.json", function(obj) {
+         for (i = 0; i < obj.classes.length; i++) {
+             if (notAquatica && aquatica) {
+                 console.log("tudo");
+             } else if (!notAquatica && aquatica) {
+                 console.log("Apenas aquatica");
+             } else if (notAquatica && !aquatica) {
+                 console.log("Apenas Não Aquatica");
+             } else {
+                 console.log("tudo");
+             }
+         } // end for
+     }); // end getJson
+ }
